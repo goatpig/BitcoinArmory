1e4d0191d6d3dae01e36d59ab224d3756a6d3eb6
Andrew Chow <achow101-github@achow101.com>
2017-06-01 13:32:30 -0700

    Replace transaction send check with a more robust one
    The original transaction send check depended on the node relaying the transaction
    back to Armory. However this behavior is not guaranteed and, due to changes in Core,
    almost never happens.
    
    The only guaranteed thing that will happen is that if the transaction is rejected for
    any reason, a reject message will be sent. If no reject message is received and the connection
    remains open and the node responsive, then the transaction was successfully sent.
    
    The transaction send check mechanism has been replaced with one that waits for a reject, and
    if it does not receive one, it will test the connection with a ping-pong.

--- cppForSwig/BDM_supportClasses.cpp
+++ cppForSwig/BDM_supportClasses.cpp
@@ -2056,42 +2056,65 @@ void ZeroConfContainer::broadcastZC(const BinaryData& rawzc,
       return;
    }
 
-   auto watchTxFuture = gds->getFuture();
+   // Wait for a reject message. If one is sent, it will go to gds
+   auto watchForRejectFut = gds->getFuture();
+   watchForRejectFut.wait_for(chrono::seconds(timeout_sec));
 
-   //try to fetch tx by hash from node
-   if(PEER_USES_WITNESS)
-      entry.invtype_ = Inv_Msg_Witness_Tx;
+   // Do a ping-pong to make sure that the connection is live
+   // If the ping-pong is successful and no reject was received, then the transaction
+   // can be considered successfully sent.
+   
+   // Get a random nonce
+   srand(time(0));
+   uint64_t nonce = rand();
+   
+   // Construct pong payload and promise
+   auto pongProm = make_shared<promise<bool>>();
+   auto pongFut = pongProm->get_future();
 
-   auto grabtxlambda = [this](InvEntry inventry)->void
-   {
-      processInvTxThread(move(inventry));
-   };
+   BitcoinP2P::pongPayload pongPayload;
+   auto&& pong_payload = make_unique<Payload_Pong>();
+   pong_payload->nonce_ = nonce;
+   pongPayload.payload_ = move(pong_payload);
+   pongPayload.promise_ = pongProm;
+   
+   pair<uint64_t, BitcoinP2P::pongPayload> pongPair;
+   pongPair.first = nonce;
+   pongPair.second = move(pongPayload);
 
-   thread grabtxthread(grabtxlambda, move(entry));
-   if (grabtxthread.joinable())
-      grabtxthread.detach();
+   //register getData payload
+   networkNode_->pongPayloadMap_.insert(move(pongPair));
 
-
+   // Construct ping payload and send
+   Payload_Ping payload_ping;
+   payload_ping.nonce_ = nonce;
+   networkNode_->sendMessage(move(payload_ping));
+   
+   //wait on pong future
    if (timeout_sec == 0)
    {
-      watchTxFuture.wait();
+      pongFut.wait();
    }
    else
    {
-      auto status = watchTxFuture.wait_for(chrono::seconds(timeout_sec));
-      if (status != future_status::ready)
+      auto pongFutStatus = pongFut.wait_for(chrono::seconds(timeout_sec));
+      if (pongFutStatus != future_status::ready)
       {
          gds->setStatus(false);
-         gds->setMessage("tx broadcast timed out (get)");
+         gds->setMessage("tx broadcast failed. Disconnected from node.");
       }
    }
-   
+      
    networkNode_->unregisterGetTxCallback(txHash);
 
    if (!gds->status())
    {
       auto&& errorMsg = gds->getMessage();
       bdv_cb.zcErrorCallback_(errorMsg, txHashStr);
+   }
+   else
+   {
+      // TODO: add tx to wallet
    }
 }
 
